54a6354d753290bcc9cd110e0c37336d0410d597
Ondra Jodas <ondrej.jodas@keboola.com>
2021-08-27 13:53:56 +0200

    used ExasolQuote

--- tests/traits/GetTablesTrait.php
+++ tests/traits/GetTablesTrait.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace Keboola\ExasolTransformation\TestTraits;
 
 use Doctrine\DBAL\Connection;
+use Keboola\TableBackendUtils\Escaping\Exasol\ExasolQuote;
 
 trait GetTablesTrait
 {
     public function getTables(Connection $connection): array
     {
-        $sqlTemplate = 'select * from EXA_ALL_TABLES WHERE "TABLE_SCHEMA" = \'%s\';';
+        $sqlTemplate = 'select * from EXA_ALL_TABLES WHERE "TABLE_SCHEMA" = %s;';
 
         return $connection->executeQuery(
-            sprintf($sqlTemplate, (string) getenv('EXASOL_SCHEMA'))
+            sprintf($sqlTemplate, ExasolQuote::quote((string) getenv('EXASOL_SCHEMA')))
         )->fetchAllAssociative();
     }
 }
